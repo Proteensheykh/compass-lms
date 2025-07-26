baaf78ec373e8daca2d423737021262746a3fe9a
Mo Sheriff <smomoh96@gmail.com>
2025-07-26 09:33:49 +0100

    feat: implement subscription limit tracking for companion creation

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,8 @@
 import CompanionForm from '@/components/CompanionForm'
+import { newCompanionPermissions } from '@/lib/actions/companion.action'
 import { auth } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
@@ -8,12 +11,26 @@ const NewCompanion = async () => {
 
   if (!userId) redirect("/sign-in")
 
+  const canCreateCompanion = await newCompanionPermissions()
+
   return (
     <main className='min-lg:w-1/3 mid-md:w-2/3 items-center justify-center'>
-      <article className='w-full gap-4 flex flex-col items-center justify-center'>
-        <h1>Companion Builder</h1>
-        <CompanionForm />
-      </article>
+      {canCreateCompanion ? (
+        <article className='w-full gap-4 flex flex-col items-center justify-center'>
+          <h1>Companion Builder</h1>
+          <CompanionForm />
+        </article>
+      ) : (
+        <article className='companion-limit'>
+          <Image src="/images/limit.svg" alt="Companion Limit Reached" width={360} height={230} />
+          <div className='cta-badge'>
+            <p>Upgrade your plan</p>
+          </div>
+          <h1>You've Reached Your Companion Limit</h1>
+          <p>Upgrade your plan to create more companions and premium features</p>
+          <Link href="/subscription" className='btn-primary w-full justify-center max-w-[200px]'>Upgrade Plan</Link>
+        </article>
+      )}
     </main>
   )
 }
